eefbed8a871c4cd58966beaab3678ff6e35196ba
Rafael Soncine <rafael.soncine@meliuz.com.br>
2022-01-20 16:48:55 -0300

    chore: Implementa lógica da função signOut

--- src/hooks/useAuth.tsx
+++ src/hooks/useAuth.tsx
@@ -82,17 +82,20 @@ function AuthProvider({ children }: AuthProviderData) {
 
   async function signOut() {
     try {
-      // set isLoggingOut to true
-
-      // call revokeAsync with access_token, client_id and twitchEndpoint revocation
+      setIsLoggingOut(true);
+      await revokeAsync(
+        {
+          token: userToken,
+          clientId: CLIENT_ID,
+        }, {
+          revocationEndpoint: twitchEndpoints.revocation
+      });
     } catch (error) {
-    } finally {
-      // set user state to an empty User object
-      // set userToken state to an empty string
-
-      // remove "access_token" from request's authorization header
-
-      // set isLoggingOut to false
+    } finally {      
+      setUser({} as User);
+      setUserToken('');
+      delete api.defaults.headers.authorization;
+      setIsLoggingOut(false);
     }
   }
 
